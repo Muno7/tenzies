1a4fa6624218b014195f6e45dac707d984a8a932
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-14 15:02:20 +0200

    Created the die component and made 10 of them randomly generated on refresh

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,26 @@
+import Die from './Die'
+import {useState} from 'react'
+
 export default function Main() {
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    const newDice = []
+
+    for (let i = 0; i < 10; i++) {
+      const randomNum = Math.ceil(Math.random() * 6)
+      newDice.push(randomNum)
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(num => <Die num={num}/>)
+
   return(
-    <main></main>    
+    <main>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+    </main>    
   )
 }
