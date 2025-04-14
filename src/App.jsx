aefbbb740fcb30e199f71330134f2d2f87d39cd9
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-14 14:06:53 +0200

    Intialized project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Main from './components/Main'
 
-function App() {
-
+export default function App() {
+  return(
+    <>
+      <Main />
+    </>
+  )   
 }
-
-export default App
