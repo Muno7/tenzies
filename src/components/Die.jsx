f4ea62905391624336524f1aa479859ba7729e25
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-14 15:32:49 +0200

    Made rolling the dice interactive

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die(props) {
   return(
-    <button className="die">
+    <button className={`die ${props.isHeld ? 'held' : ''}`}>
       {props.num}
     </button>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,41 @@
 import Die from './Die'
 import {useState} from 'react'
+import { nanoid } from 'nanoid'
 
 export default function Main() {
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState([])
 
   function generateAllNewDice() {
     const newDice = []
 
     for (let i = 0; i < 10; i++) {
       const randomNum = Math.ceil(Math.random() * 6)
-      newDice.push(randomNum)
+      newDice.push({
+        id: nanoid(),
+        value: randomNum,
+        isHeld: false
+      })
     }
     return newDice
   }
 
-  const diceElements = dice.map(num => <Die num={num}/>)
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map(dieObj => <Die key={dieObj.id} isHeld={dieObj.isHeld} num={dieObj.value}/>)
 
   return(
     <main>
       <div className='dice-container'>
         {diceElements}
       </div>
+      <button 
+        className="roll"
+        onClick={rollDice}
+      >
+        Roll
+      </button>
     </main>    
   )
 }
